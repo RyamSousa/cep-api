bb9388f9b961d0c43113fdca10704fa49f70b86e
RyamSousa <ryam.sousa123@gmail.com>
2023-02-20 23:19:39 -0300

    :sparkles: added swagger documentation

--- src/main/java/com/demo/cepapi/api/v1/CepController.java
+++ src/main/java/com/demo/cepapi/api/v1/CepController.java
@@ -1,15 +1,24 @@
-package com.demo.cepapi.api;
+package com.demo.cepapi.api.v1;
 
 import com.demo.cepapi.domain.request.AddressRequest;
 import com.demo.cepapi.domain.response.AddressResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Api(value = "CEP Rest API")
 public interface CepController {
 
+    @ApiOperation(value = "Calculate freight and return address data")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return freight value and the address informations"),
+            @ApiResponse(code = 404, message = "CEP not found")})
     @PostMapping(value = "/v1/consulta-endereco", produces = APPLICATION_JSON_VALUE)
     ResponseEntity<AddressResponse> getAddressByCep(@RequestBody AddressRequest addressRequest);
 }
--- src/main/java/com/demo/cepapi/api/v1/impl/CepControllerImpl.java
+++ src/main/java/com/demo/cepapi/api/v1/impl/CepControllerImpl.java
@@ -1,15 +1,13 @@
-package com.demo.cepapi.api.impl;
+package com.demo.cepapi.api.v1.impl;
 
-import com.demo.cepapi.api.CepController;
+import com.demo.cepapi.api.v1.CepController;
 import com.demo.cepapi.domain.request.AddressRequest;
 import com.demo.cepapi.domain.response.AddressResponse;
 import com.demo.cepapi.service.impl.CepServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class CepControllerImpl implements CepController {
 
